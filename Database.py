e4e3c2dc2cd707286d86979b6edeb378ea29176a
Noah <122134974+doctorwizard23@users.noreply.github.com>
2023-10-01 15:39:35 -0700

    Create Database.py

---
+++ Database.py
@@ -0,0 +1,51 @@
+import pymssql
+
+
+class Database:
+    __connection = None
+
+    @classmethod
+    def __connect(cls):
+        """
+        Connects to pymssql database
+        :return: none
+        """
+        if cls.__connection is None:
+            cls.__connection = pymssql.connect(server='cisdbss.pcc.edu', user='275student', password='275student',
+                                               database='NAMES')
+
+
+    @classmethod
+    def retrieveNames(cls, Name, Gender):
+        """
+        Retrieves data from database according to name and gender
+        :param Name: string, name input to search database for
+        :param Gender: string, gender M or F to search database for
+        :return: namesList, List of data retrieved from database
+        """
+        namesList = []
+
+        if (cls.__connection is None):
+            cls.__connect()
+
+        myCursor = cls.__connection.cursor()
+        myCursor.execute(
+            "SELECT top 100 * From all_data WHERE Name=%s AND Gender=%s",
+            (Name, Gender))
+
+
+        namesList = myCursor.fetchall()
+
+        return namesList
+
+
+
+
+
+
+
+
+
+
+
+
